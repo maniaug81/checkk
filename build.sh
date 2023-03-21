3365ba72178aef7ec2e47563246b278c3f99f565
maniaug81 <maniaug81@gmail.com>
2023-03-21 19:30:00 +0530

    updated build.sh fix-jira220

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t maniaug81/my-custom-todo:1.0 .
+docker build -t maniaug81/my-custom-todo:$1 .
 docker rm -f $(docker ps -aqf "name=mycustomtodo")
-docker run -it --name mycustomtodo -p 8082:80 -d maniaug81/my-custom-todo:1.0
+docker run -it --name mycustomtodo -p 8082:80 -d maniaug81/my-custom-todo:$1
